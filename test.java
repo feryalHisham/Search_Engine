455d38f12b7dbc30407dea4b69c119ef894df206
HebaAhmedAli <36989462+HebaAhmedAli@users.noreply.github.com>
2018-03-12 07:09:45 -0400

    Add files via upload

--- test.java
+++ test.java
@@ -1,11 +1,7 @@
 
 
 import com.mongodb.*;
-import com.mongodb.util.JSON;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
 
 
 public class test {
@@ -14,12 +10,11 @@ public class test {
     public static void main (String[] args)throws Exception {
 
         DB db=null;
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
 
 
             MongoClient mongoClient = new MongoClient("localhost", 27017);
-            db = mongoClient.getDB("textStemming");
+            db = mongoClient.getDB("search_engine");
             System.out.println("Connected to Database");
 
 
@@ -32,21 +27,92 @@ public class test {
        System.out.println("Server is ready ");
 
 
-        DBCollection collection = db.getCollection("collection");
-        BasicDBObject document = new BasicDBObject();
-        document.put("hosting", "hostA");
-        document.put("type", "vps");
-        document.put("clients", 1000);
-        collection.insert(document);
+        BasicDBObject theWord = new BasicDBObject();
 
-        System.out.println("after insertion");
-        DBCursor dbCursor = collection.find();
-        while(dbCursor.hasNext()){
-            System.out.println(dbCursor.next());
-        }
+        theWord.put("word","german");
 
 
+        DBCollection collection = db.getCollection("wordsIndex");
+/*
+*
+* nm7 kol ele y7os l url
+* */
+        String SURL="https://www.geeksforgeeks.org/";
+        BasicDBObject q1 = new BasicDBObject();
+        BasicDBObject q2 = new BasicDBObject();
+        BasicDBObject q3 = new BasicDBObject();
 
+        q1.put("url",SURL);
+        q2.put("",q1);
+        q3.put("urls",q2);
+
+        DBObject update_idf = new BasicDBObject();
+        update_idf.put("$inc", new BasicDBObject("idf", -1));
+        collection.updateMulti(q3, update_idf);
+        
+        DBObject update = new BasicDBObject();
+		update.put("$pull", q3);
+		collection.updateMulti(q3, update);
+		
+		
+         System.out.println("5alst ya bashr...");
+        /*DBCursor result = collection.find(q3);
+        while (result.hasNext()) {
+            BasicDBObject querry = (BasicDBObject) result.next();//new BasicDBObject("url",SURL);
+            DBObject update_idf = new BasicDBObject();
+            update_idf.put("$inc", new BasicDBObject("idf", -1));
+            collection.updateMulti(querry, update_idf);
+
+        }*/
+      /*  BasicDBObject b1 = new BasicDBObject();
+        BasicDBObject b2 = new BasicDBObject();
+        BasicDBObject b3 = new BasicDBObject();
+        BasicDBObject b4 = new BasicDBObject();
+        BasicDBObject b5 = new BasicDBObject();
+
+        b2.put("url",SURL );
+        b3.put("urls",b2);
+        b4.put("$pull",b3);
+        b5.put("multi","true");
+
+
+        collection.update(b1,b4,false,true);
+
+
+
+//        DBCursor dbCursor = collection.find(theWord);
+//        if (dbCursor.hasNext()){
+//
+//        }
+//        BasicDBObject idfinc = new  BasicDBObject().append("$inc",
+//                new BasicDBObject().append("idf", 1));
+//
+//        collection.update(new BasicDBObject().append("word","german"),idfinc);
+//
+//
+//        List<BasicDBObject> occurence = new ArrayList<>();
+//
+//
+//            BasicDBObject occurenceTag = new BasicDBObject();
+//            occurenceTag.put("tagName", "h1");
+//            occurenceTag.put("numOccur", 16);
+//            occurence.add(occurenceTag);
+//
+//        BasicDBObject occurenceTag2 = new BasicDBObject();
+//        occurenceTag2.put("tagName", "pre");
+//        occurenceTag2.put("numOccur", 29);
+//        occurence.add(occurenceTag2);
+//
+//
+
+//        BasicDBObject urlObject = new BasicDBObject();
+//        urlObject.put("url", "https://bos_b2a_3l4an_tb2a_3arf");
+//
+//        BasicDBObject tempisa = new BasicDBObject();
+//        tempisa.put("$push", new BasicDBObject().append("urls", urlObject));
+//        collection.update(new BasicDBObject().append("word","shallow"),tempisa);
+
+*/
     }
 
 
